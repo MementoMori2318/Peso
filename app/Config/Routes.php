1fffb831a976bef469ec25f75c31752395ce3c16
Mencero <micmencero@my.cspc.edu.ph>
2025-05-17 01:03:19 +0800

    google sign in

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('jobs', 'Jobs::index');
+$routes->get('/', 'Jobs::index');
+
+//sign in route
+$routes->get('/signin', 'AuthController::signinPage');
+$routes->get('/auth/google', 'AuthController::googleSignIn');
---
+++ app/Views/signin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In with Google</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+</head>
+<body>  
+    <a href="/jobs" class="button is-primary">home</a>
+        <div class="container">
+            <h1 class="title">Sign In with Google</h1>
+            <a href="/auth/google" class="button is-primary">Sign In with Google</a>
+        </div>
+    </section>
+</body>
+</html>
